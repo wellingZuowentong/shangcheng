9a71b5c97206392a557471ace2b1184ffd406f34
wanghuaipeng666 <1055665333@qq.com>
2019-08-09 18:03:56 +0800

    增加注释，修改退货，添加评价状态

---
+++ src/main/java/com/shangcheng/shangcheng/controller/KuaiDiController.java
@@ -0,0 +1,8 @@
+package com.shangcheng.shangcheng.controller;
+
+/**
+ * 作者：王怀朋
+ * 日期：2019/8/9
+ */
+public class KuaiDiController {
+}
---
+++ src/main/java/com/shangcheng/shangcheng/mapper/KuaiDiMapper.java
@@ -0,0 +1,8 @@
+package com.shangcheng.shangcheng.mapper;
+
+/**
+ * 作者：王怀朋
+ * 日期：2019/8/9
+ */
+public interface KuaiDiMapper {
+}
